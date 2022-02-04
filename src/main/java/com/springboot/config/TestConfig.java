a7203672ef3fb1820187c9bb19c71ecfa6944e3d
all-an <all-an@hotmail.com>
2022-02-04 10:31:11 -0300

    Payment, one-to-one association

--- src/main/java/com/springboot/config/TestConfig.java
+++ src/main/java/com/springboot/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.springboot.entities.Category;
 import com.springboot.entities.Order;
 import com.springboot.entities.OrderItem;
+import com.springboot.entities.Payment;
 import com.springboot.entities.Product;
 import com.springboot.entities.User;
 import com.springboot.entities.enums.OrderStatus;
@@ -74,11 +75,16 @@ public class TestConfig implements CommandLineRunner {
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
 		
-		OrderItem oi1 = new OrderItem(o1, p1, 2); //, p1.getPrice()
-		OrderItem oi2 = new OrderItem(o1, p3, 1);
-		OrderItem oi3 = new OrderItem(o2, p3, 2);
-		OrderItem oi4 = new OrderItem(o3, p5, 2);
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice()); //, p1.getPrice()
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p2.getPrice());
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p4.getPrice());
 
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));	
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+
+		orderRepository.save(o1);
 	}
 }
--- src/main/java/com/springboot/entities/OrderItem.java
+++ src/main/java/com/springboot/entities/OrderItem.java
@@ -23,12 +23,12 @@ public class OrderItem implements Serializable {
 	public OrderItem() {
 	}
 
-	public OrderItem(Order order, Product product, Integer quantity) { //, Double price
+	public OrderItem(Order order, Product product, Integer quantity, Double price) { //, Double price
 		super();
 		id.setOrder(order);
 		id.setProduct(product);
 		this.quantity = quantity;
-		this.price = (double)quantity * product.getPrice();
+		this.price = price; //(double)quantity * product.getPrice();
 	}
 
 	@JsonIgnore
